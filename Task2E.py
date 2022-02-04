50ce402a16b231203fc9e78b32fdf9df05d04595
CalumStevenson <crbs2@cam.ac.uk>
2022-02-04 13:34:55 +0000

    Task 2E Complete

--- Task2E.py
+++ Task2E.py
@@ -2,15 +2,12 @@
 from floodsystem.stationdata import build_station_list
 from floodsystem.plot import plot_water_levels
 from floodsystem.utils import fetch_station_list_levels
-import datetime
-from tqdm import tqdm
-
-stations = build_station_list()
-topTenStations = fetch_station_list_levels(stations,10,10)
-for station in topTenStations:
-    plot_water_levels(station,station.level_history[0],station.level_history[1])
-"""   
-station = stations[0]
-dt = 2
-dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt))
-plot_water_levels(station,dates,levels)"""
+#You must pip install tqdm!!!
+def run():
+    stations = build_station_list()
+    topTenStations = fetch_station_list_levels(stations,10,10)
+    for station in topTenStations:
+        plot_water_levels(station,station.level_history[0],station.level_history[1])
+    
+if __name__ == "__main__":
+    run()
