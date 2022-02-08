a015246b2703474c19f275d84a3ee4f71ee0eb66
CalumStevenson <crbs2@cam.ac.uk>
2022-02-08 14:53:27 +0000

    Milestone 1

--- Task1C.py
+++ Task1C.py
@@ -1,9 +1,13 @@
 from floodsystem import geo
 from floodsystem import stationdata
-stations = stationdata.build_station_list()
-centre = 52.2053, 0.1218
-r = 10
-closest_stations = geo.stations_within_radius(stations, centre, r)
+def run():
+    stations = stationdata.build_station_list()
+    centre = 52.2053, 0.1218
+    r = 10
+    closest_stations = geo.stations_within_radius(stations, centre, r)
 
-closest_stations.sort()
-print(closest_stations)
+    closest_stations.sort()
+    print(closest_stations)
+
+if __name__ == "__main__":
+    run()
